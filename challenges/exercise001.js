71f00a8d94f5f012a10fc82dfe58b4fe9aa75b81
deedeeh <dinahafez.8@gmail.com>
2022-02-07 23:17:14 +0000

    Implemented functionality for addVAT

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -12,7 +12,13 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  const vatPrice = originalPrice * (vatRate / 100);
+  const priceAfterVAT = originalPrice + vatPrice;
+  if (priceAfterVAT - Math.floor(priceAfterVAT) !== 0) {
+    return parseFloat(priceAfterVAT.toFixed(2));
+  } else {
+    return priceAfterVAT;
+  }
 }
 
 function getSalePrice(originalPrice, reduction) {
