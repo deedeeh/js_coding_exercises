b8725881c21fb9f5f6afcbad050d2907e35a0def
deedeeh <dinahafez.8@gmail.com>
2022-02-08 22:51:30 +0000

    Implemented functionality of hasMPostCode

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -28,7 +28,7 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester";
 }
 
 module.exports = {
