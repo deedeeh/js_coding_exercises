1e549b892c559b2f067174f9f0a081b180532811
deedeeh <dinahafez.8@gmail.com>
2022-02-09 22:17:58 +0000

    Refactored exercise 4 functions using forEach, map and filter methods

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,83 +1,62 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  const smallNums = [];
-  for(let i = 0; i < nums.length; i++) {
-    if(nums[i] < 1) {
-      smallNums.push(nums[i]);
-    }
-  }
-  return smallNums;
+  return nums.filter(num => num < 1)
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  const foundNames = [];
-  for(let i = 0; i < names.length; i++) {
-    if(names[i].substring(0, 1) === char) {
-      foundNames.push(names[i]);
-    }
-  }
-  return foundNames;
+  return names.filter(name => name.substring(0,1) === char);
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  const foundVerbs = [];
-  for(let i = 0; i < words.length; i++) {
-    if(words[i].substring(0, 3) === "to ") {
-      foundVerbs.push(words[i]);
-    }
-  }
-  return foundVerbs;
+  return words.filter(word => word.substring(0,3) === "to ")
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  const onlyIntegers = [];
-  for(let i = 0; i < nums.length; i++) {
-    if(nums[i] === parseInt(nums[i])) {
-      onlyIntegers.push(nums[i]);
-    }
-  }
-  return onlyIntegers;
+  return nums.filter(num => num === parseInt(num))
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
   const cities = [];
-  for(let i = 0; i < users.length; i++) {
-    cities.push(users[i].data.city.displayName)
-  }
+  users.forEach(user => 
+    cities.push(user.data.city.displayName)
+  )
   return cities;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  const squareRoots = [];
-  for(let i = 0; i < nums.length; i++) {
-    squareRoots.push(parseFloat(Math.sqrt(nums[i]).toFixed(2)))
-  }
-  return squareRoots;
+  return nums.map(num => parseFloat(Math.sqrt(num).toFixed(2)))
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  const foundSentence = [];
   const capitalizedStr = str.substring(0,1).toUpperCase() + str.substring(1)
-  for(let i = 0; i < sentences.length; i++) {
-    if(sentences[i].includes(str) || sentences[i].includes(capitalizedStr)) {
-      foundSentence.push(sentences[i]);
+  return sentences.filter(sentence => {
+    if(sentence.includes(str) || sentence.includes(capitalizedStr)) {
+      return sentence
     }
-  }
-  return foundSentence;
+  })
 }
 
+//Still trying to figure it out 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  // const longestSides = [];
+  // for(let i = 0; i < triangles.length; i++) {
+  //   for(let j = 0; j < triangles[i].length; j++) {
+  //     triangles[i][j]
+  //   }
+  // }
+  // return longestSides;
 }
+
+
 
 module.exports = {
   findSmallNums,
